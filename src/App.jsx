0cbd53d04c069007ffbbda2b39bf6990a1093554
Ankit <ankitkeshri0727@gmai;.com>
2023-03-30 22:12:53 +0530

    added TicTacToe game title and included Roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,94 @@
-import './style.scss'
-import {useState} from 'react'
-import Board from './components/Board'
+import './style.scss';
+import { useState } from 'react';
+import Board from './components/Board';
 import StatusMessage from './components/statusMessage';
 import History from './components/History';
 import { calculateWinner } from './winner';
 
-const NEW_GAME = [{squares: Array(9).fill(null), isXNext : false}]
-
+const NEW_GAME = [{ squares: Array(9).fill(null), isXNext: false }];
 
 function App() {
   const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
 
-  const gamingBoard = history[currentMove]
+  const gamingBoard = history[currentMove];
 
-  
-  const {winner, winningSquares} = calculateWinner(gamingBoard.squares);
-
+  const { winner, winningSquares } = calculateWinner(gamingBoard.squares);
 
   const handleSquareClick = clickedPosition => {
-
-    if(gamingBoard.squares[clickedPosition] || winner){
-        return;
+    if (gamingBoard.squares[clickedPosition] || winner) {
+      return;
     }
 
-    setHistory((currentHistory)=>{
-        const isTraversing = currentMove+1 !== currentHistory.length;
+    setHistory(currentHistory => {
+      const isTraversing = currentMove + 1 !== currentHistory.length;
 
-        const lastGamingState =isTraversing? currentHistory[currentMove]: currentHistory[currentHistory.length-1]
+      const lastGamingState = isTraversing
+        ? currentHistory[currentMove]
+        : currentHistory[currentHistory.length - 1];
 
+      const nextSquaresState = lastGamingState.squares.map(
+        (squareValue, position) => {
+          if (clickedPosition === position) {
+            return lastGamingState.isXNext ? 'X' : '0';
+          }
+          return squareValue;
+        }
+      );
 
-        const nextSquaresState = lastGamingState.squares.map((squareValue, position) =>{
-            if(clickedPosition===position){
-                return lastGamingState.isXNext?'X':'0';
-            }
-            return squareValue
-        })
+      const base = isTraversing
+        ? currentHistory.slice(0, currentHistory.indexOf(lastGamingState) + 1)
+        : currentHistory;
 
-
-        const base = isTraversing?currentHistory.slice(0, currentHistory.indexOf(lastGamingState)+1):currentHistory;
-
-        return base.concat({squares: nextSquaresState , isXNext : !lastGamingState.isXNext,
-        })
+      return base.concat({
+        squares: nextSquaresState,
+        isXNext: !lastGamingState.isXNext,
+      });
     });
 
-    setCurrentMove(move => move+1)
+    setCurrentMove(move => move + 1);
   };
 
-  const moveTo = move =>{
-    setCurrentMove(move)
-  }
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
 
-  const onNewGameStart = () =>{
+  const onNewGameStart = () => {
     setHistory(NEW_GAME);
     setCurrentMove(0);
-  }
+  };
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
+
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
-      <Board squares={gamingBoard.squares} handleSquareClick = {handleSquareClick}
+      <Board
+        squares={gamingBoard.squares}
+        handleSquareClick={handleSquareClick}
         winningSquares={winningSquares}
       />
 
+      <button
+        type="button"
+        onClick={onNewGameStart}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start new game
+      </button>
 
-      <button type="button" onClick={onNewGameStart} className={
-        `btn-reset ${ winner ? 'active' : '' }`
-      }>Start new game</button>
-
-
-      <h2>Current Game History</h2>
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current Game History
+      </h2>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
